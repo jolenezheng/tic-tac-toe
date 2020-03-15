e4061937ffcb6470d831b76b6098b3e170a2b829
Jolene Zheng <jolenezheng18@gmail.com>
2020-03-15 19:45:48 -0400

    check win logic complete

--- main.py
+++ main.py
@@ -19,10 +19,142 @@ def print_board(game_board):
                 print('- ', end='')
         print()
 
-def check_win(game_board, last_move, turn):
+def check_win(game_board, player, last_move, turn):
+    index = last_move[1]
+    matches = 0
+    player_num = 2 # X
+
+    if player == 'O':
+        player_num = 3
+
+    # Check row:
+    # Left of curr x pos
+    while (index - 1) >= 0:
+        if game_board[last_move[0]][index - 1] == player:
+            matches += 1
+            index -= 1
+        else:
+            break
+
+        if matches == 2:
+            return player_num
+
+
+    # Right of curr x pos
+    index = last_move[1]
+    while (index + 1) <= 2:
+        if game_board[last_move[0]][index + 1] == player:
+            matches += 1
+            index += 1
+        else:
+            break
+
+        if matches == 2:
+            return player_num
+
+
+    matches = 0
+
+    #Check col:
+    # Above curr y pos
+    index = last_move[0]
+    while (index - 1) >= 0:
+        if game_board[index - 1][last_move[1]] == player:
+            matches += 1
+            index -= 1
+        else:
+            break
+    
+        if matches == 2:
+            return player_num
+
+
+    # Below curr y pos
+    index = last_move[0]
+    while (index + 1) <= 2:
+        if game_board[index + 1][last_move[1]] == player:
+            matches += 1
+            index += 1
+        else:
+            break
+        
+        if matches == 2:
+            return player_num
+
+
+    matches = 0
+
+    # Check diagonal: top left to bottom right
+    # Towards top left
+    index_row = last_move[0]
+    index_col = last_move[1]
+    while (index_row - 1) >= 0 and (index_col - 1) >= 0:
+        if game_board[index_row - 1][index_col - 1] == player:
+            matches += 1
+            index_row -= 1
+            index_col -= 1
+        else:
+            break
+
+        if matches == 2:
+            return player_num
+
+
+    # Towards bottom right
+    index_row = last_move[0]
+    index_col = last_move[1]
+    while (index_row + 1) <= 2 and (index_col + 1) <= 2:
+        if game_board[index_row + 1][index_col + 1] == player:
+            matches += 1
+            index_row += 1
+            index_col += 1
+        else:
+            break
+
+        if matches == 2:
+            return player_num
+
+
+    matches = 0
+
+    # Check diagonal: top right to bottom left
+    # Towards top right
+    index_row = last_move[0]
+    index_col = last_move[1]
+    while (index_row - 1) >= 0 and (index_col + 1) <= 2:
+        if game_board[index_row - 1][index_col + 1] == player:
+            matches += 1
+            index_row -= 1
+            index_col += 1
+        else:
+            break
+
+        if matches == 2:
+            return player_num
+
+
+    # Towards bottom left
+    index_row = last_move[0]
+    index_col = last_move[1]
+    while (index_row + 1) <= 2 and (index_col - 1) >= 0:
+        if game_board[index_row + 1][index_col - 1] == player:
+            matches += 1
+            index_row += 1
+            index_col -= 1
+        else:
+            break
+
+        if matches == 2:
+            return player_num
+
+
+    # If there's no winner yet and the grid isn't filled yet continue the game.
     if turn < 8:
         return 0
-    return 1    
+
+    # If no winner is detected by this point, it's a tie game.
+    return 1
+
 
 while win_detected == 0:
     print()
@@ -46,7 +178,8 @@ while win_detected == 0:
     game_board[move_row][move_col] = player
     print_board(game_board)
 
-    win_detected = check_win(game_board, (move_row, move_col), turn) # change tuple to be last move
+    win_detected = check_win(game_board, player, (move_row, move_col), turn) # change tuple to be last move
+
     if win_detected == 1:
         print('Tie Game!')
     elif win_detected == 2:
